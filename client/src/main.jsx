392c500f9311cfca6f5356ee808eb7d9c2aaf0a0
patilmahesh007 <patilmahesh4007>
2025-01-18 20:53:09 +0530

    view controller folder added routes added for login and signup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
-
+import Signup from './views/signup/signup.jsx';
+import Login from './views/login/login.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </BrowserRouter>
 );
