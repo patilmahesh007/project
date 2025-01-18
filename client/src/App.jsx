3149a6888dbed1175b04ea85a563fa863ab18b4e
patilmahesh007 <patilmahesh4007>
2025-01-18 20:49:28 +0530

    tailwind basic setup added favicon create react app using vite also routingsetup

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+function App() {
+  return (
+    <div className="w-screen h-screen bg-slate-700 text-red-700 text-4xl flex justify-center items-center">App</div>
+  )
+}
+
+export default App
