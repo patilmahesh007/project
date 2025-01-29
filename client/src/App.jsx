1bd7a45a53ecd03903c40347a398e00999179925
patilmahesh007 <patilmahesh4007>
2025-01-29 23:12:50 +0530

    added route for login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 function App() {
   return (
-    <div className="w-screen h-screen bg-slate-700 text-red-700 text-4xl flex justify-center items-center">App</div>
+    <div className="w-screen h-screen bg-slate-700 text-red-700 text-4xl flex justify-center items-center">App
+    <Link to={'/login'}>login</Link>
+    </div>
   )
 }
 
