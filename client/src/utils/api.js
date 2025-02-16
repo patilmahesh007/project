ff9f233221d9bd915f8b5f4271e92dd0336e9351
patilmahesh007 <patilmahesh4007>
2025-02-16 15:08:58 +0530

    vercel config rewrites

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-
 const api = axios.create({
-  baseURL: '/api',  // Now same-origin as your frontend
-  withCredentials: true,
-});
+    baseURL: import.meta.env.VITE_BASE_URL,
+    withCredentials: true
+  });
 
-export default api;
+  export default api
