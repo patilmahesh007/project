ead5aad207144f9c45e4e973f2c040bf239a8314
patilmahesh007 <patilmahesh4007>
2025-03-06 20:14:13 +0530

    cors change

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://project-1121.onrender.com", 
   credentials: true,
 }));
 
