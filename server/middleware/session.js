b5888570f44295e5729560989eb6e11bd9864528
patilmahesh007 <patilmahesh4007>
2025-03-12 14:41:01 +0530

    added domain attribute

--- server/middleware/session.js
+++ server/middleware/session.js
@@ -10,7 +10,7 @@ const sessionMiddleware = session({
     secure: process.env.NODE_ENV === "production",
     sameSite: "none", 
     maxAge: 24 * 60 * 60 * 1000,
-    domain: process.env.NODE_ENV === "production" ? ".onrender.com" : undefined,
+    domain: process.env.NODE_ENV=== "production" ? ".onrender.com" : undefined,
     path: "/",
   },
 });
